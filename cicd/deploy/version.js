6a1303f8cb9fa04ddbd85dfec182311fe05bcf34
magnuspaal <magnuspaal@gmail.com>
2023-08-27 15:22:11 +0300

    ci: change image location and tag

--- cicd/deploy/version.js
+++ cicd/deploy/version.js
@@ -4,7 +4,7 @@ const compose = './cicd/deploy/docker-compose.yml';
 const pom = './pom.xml'
 const newVersion = process.argv[2]
 
-bumpVersion(compose, /^(\s+image.*file-server:)(.+)$/m, newVersion);
+bumpVersion(compose, /^(\s+image.*other:file-server-)(.+)$/m, newVersion);
 bumpVersion(pom, /^(\s+<artifactId>file-server<\/artifactId>\n\s+<version>)(.+?(?=<\/version>))/m, newVersion);
 
 function bumpVersion(file, regexp, version) {
